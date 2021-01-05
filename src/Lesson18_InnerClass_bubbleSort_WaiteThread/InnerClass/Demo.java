72cfaa5f94b1621b3e7a592c06a9756a57e0ab47
Maryam-ask <maryamaskari19@gmail.com>
2021-01-05 15:14:16 +0100

    Inner-Classes added in Lesson18.

---
+++ src/Lesson18_InnerClass_bubbleSort_WaiteThread/InnerClass/Demo.java
@@ -0,0 +1,14 @@
+package Lesson18_InnerClass_bubbleSort_WaiteThread.InnerClass;
+
+/**
+ * Created by Maryam Askari
+ * Date: 12/30/2020
+ * Time: 8:55 PM
+ * Project: IntelliJ IDEA
+ */
+public class Demo {
+
+    public void show(){
+        System.out.println("I am in super class");
+    }
+}
